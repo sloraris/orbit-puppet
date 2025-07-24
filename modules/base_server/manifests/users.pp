100f216de4df0da901e3ccb5fa9a49e4cc9d92e1
sloraris <97694636+sloraris@users.noreply.github.com>
2025-07-24 14:49:13 -0600

    moved lookup for users list inside the class

--- modules/base_server/manifests/users.pp
+++ modules/base_server/manifests/users.pp
@@ -1,7 +1,6 @@
 # modules/base_server/manifests/users.pp
-class base_server::users (
-  Array[String] $base_users = lookup('base_server::users::base_users'),
-) {
+class base_server::users {
+  $base_users = lookup('base_server::users::base_users', Array[String], 'unique', ['orbit'])
 
   # Ensure default users exist
   $base_users.each |String $user| {
